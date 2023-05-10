dd2ec20b11db19fe07f5de2b9407bf5c9cf95410
DavidUdic-P1A <115144133+DavidUdic-P1A@users.noreply.github.com>
2023-05-10 22:18:34 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,47 @@
 radio.setGroup(213)
 radio.setFrequencyBand(7)
 
-radio.onReceivedNumber(function (receivedNumber) {
-    console.log(receivedNumber)
-})
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(1)
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(2)
-})
 
-input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(3)
-})
+const directionState = {
+    STOP: 0,
+    FORWARD: -1,
+    BACKWARD: 1,
+    LEFT: -2,
+    RIGHT: 2
+}
 
-radio.onReceivedValue(function (name, value) {
+let Xdir = directionState.STOP
+let Zdir = directionState.STOP
 
-})
-radio.onReceivedValue(function (name: string, value: number) {
-    console.log(name)
-    console.log(value)
+let lastXdir = directionState.STOP
+let lastZdir = directionState.STOP
+
+basic.forever(function () {
+    let pitch = input.rotation(Rotation.Pitch)
+    let roll = input.rotation(Rotation.Roll)
+    if (roll >= 45) {
+        Xdir = directionState.RIGHT
+    } else if (roll <= -45) {
+        Xdir = directionState.LEFT
+    } else {
+        Xdir = directionState.STOP
+    }
+    if (pitch >= 45) {
+        Zdir = directionState.BACKWARD
+    } else if (pitch <= -45) {
+        Zdir = directionState.FORWARD
+    } else {
+        Zdir = directionState.STOP
+    }
+
+    if (Xdir != lastXdir) {
+        radio.sendValue("Xdir",Xdir)
+    }
+
+    if (Zdir != lastZdir) {
+        radio.sendValue("Zdir", Zdir)
+    }
+
+    lastXdir = Xdir
+    lastZdir = Zdir
 })
